7069e4e0a3bf05cfe9b9593a53cf5e410c245caf
ne0rrmatrix <ne0rmatrix@gmail.com>
2024-07-14 13:53:34 -0700

    Update AppShell and Android back behavior
    - Removed `Title` property from `AppShell.xaml`.
    - Updated `Shell.NavBarIsVisible` for platform-specific visibility in `AppShell.xaml`.
    - Added Android-specific back button override in `AppShell.xaml.cs`.
    - Updated `ApplicationDisplayVersion` in `TwitLive.csproj` to `0.1`.
    - Removed redundant `Shell.NavBarIsVisible` and added `Shell.BackgroundColor` in `ShowPage.xaml`.

--- TwitLive/AppShell.xaml.cs
+++ TwitLive/AppShell.xaml.cs
@@ -10,4 +10,18 @@ public partial class AppShell : Shell
         Routing.RegisterRoute("//ShowPage", typeof(ShowPage));
         Routing.RegisterRoute("//VideoPlayerPage", typeof(VideoPlayerPage));
     }
+#if ANDROID
+    protected override bool OnBackButtonPressed()
+    {
+        // The Windows back button calls Shell.OnBackButtonPressed and then invokes Page.BackButtonBehvior, Android does not. 
+        BackButtonBehavior? bb = CurrentPage.GetPropertyIfSet(BackButtonBehaviorProperty, returnIfNotSet: null as BackButtonBehavior);
+        if (bb is not null)
+        {
+            if (bb.IsVisible && bb.IsEnabled && bb.Command != null)
+                bb.Command?.Execute(null);
+            return true; // do not do anything further
+        }
+        return base.OnBackButtonPressed();
+    }
+#endif
 }
