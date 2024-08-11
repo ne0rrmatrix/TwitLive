cdb42066275fb42dcee9838730346dd1ad2cb285
James Crutchley <ne0rmatrix@gmail.com>
2024-08-11 10:59:11 -0700

    YOUR_COMMIT_MESSAGE

--- TwitLive/Views/VideoPlayerPage.xaml.cs
+++ TwitLive/Views/VideoPlayerPage.xaml.cs
@@ -15,7 +15,6 @@ public partial class VideoPlayerPage : ContentPage, IDisposable
 
 	Show? show;
 	IDb? item;
-
 	public VideoPlayerPage(VideoPlayerViewModel viewModel)
 	{
 		InitializeComponent();
